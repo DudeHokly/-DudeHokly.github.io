6314908a4c893ad92db6981da3fe2edf4e28b630
DudeHokly <ursus12334@gmail.com>
2025-03-10 00:33:28 +0300

    changed chunkSizeWarningLimit: 1000

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Увеличиваем лимит до 1000 кБ (1 МБ)
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
